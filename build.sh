76399f17021fa5f6522ac89c4457ce234ba0e612
Sajid Anwar <sajidanwar94@gmail.com>
2022-02-20 13:15:40 -0700

    Add support for multiple configurations, and pre/post hooks

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-echo "Building project $1:$2 with MPLAB X v5.45 and XC8 v1.34"
+PROJECT=$1
+CONFIGURATIONS=$2
+PRE=$3
+POST=$4
 
-set -x -e
+set -e
+echo "Building project $PROJECT for configurations '$CONFIGURATIONS' with MPLAB X v6.00 and XC8 v2.36"
 
-/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh "$1@$2" || exit 1
-make -C "$1" CONF="$2" build || exit 2
+if [ ! -z "$PRE" ]; then
+	eval $PRE
+fi
+
+for CONFIGURATION in ${CONFIGURATIONS}; do
+	echo "Generating Makefiles for configuration $CONFIGURATION"
+	/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh "$PROJECT@$CONFIGURATION" || exit 1
+
+	echo "Building configuration $CONFIGURATION"
+	make -C "$PROJECT" CONF="$CONFIGURATION" build || exit 2
+done
+
+if [ ! -z "$POST" ]; then
+	eval $POST
+fi
